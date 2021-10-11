20b26faf8f87d2d0bfcd3bc8ece5fa452cc5fa5f
Annika Klüpfel <AnnikaKluepfel@web.de>
2021-10-11 23:58:45 +0200

    encoding detection solution with chardet module

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 from flask import Flask, render_template, request, redirect, send_file
 app = Flask(__name__)
 
+import chardet
 from compare import runComparison, saveToFile
 
 app.config["ALLOWED_FILE_EXTENSIONS"] = ["CSV"]
@@ -35,33 +36,55 @@ def upload():
                 print("At least one file extension is invalid. Please upload two .csv files!")
                 return "At least one file extension is invalid. Please upload two .csv files!"
 
-            def decode(file):
-                result = pd.DataFrame()
-                encoding = ['utf8', 'iso-8859-1', 'ascii', 'latin1']
-                print(file.name)
-                for enc in encoding:
-                    try:
-                        result = pd.read_csv(file, sep='[:;,]', engine='python', encoding=enc) # supported delimiters: : - ; - ,
-                        print(type(result))
-                        if not result.empty:
-                            print(enc, result)
-                            break
-                    except TypeError:
-                        print('Type error with enc format: ', enc)
-                        continue
-                    except NameError:
-                        print('Name error with enc format: ', enc)
-                        continue
-                    except UnicodeDecodeError:
-                        print('Unicode decode error with enc format: ', enc)
-                        continue
-                    except:
-                        print('File could not be read with enc format: ', enc)
-                        continue
-                return result
+            # ---- DECODE, 1st ATTEMPT: trying different encoding formats with a loop --------
+            
+            # def decode(file):
+            #     result = pd.DataFrame()
+            #     print(file.name)
+ 
+                # encoding = ['utf8', 'iso-8859-1', 'ascii', 'latin1']
+                # for enc in encoding:
+                #     try:
+                #         result = pd.read_csv(file, sep='[:;,]', engine='python', encoding=enc) # supported delimiters: : - ; - ,
+                #         print(type(result))
+                #         if not result.empty:
+                #             print(enc, result)
+                #             break
+                #     except IndexError:
+                #         print('Index error with enc format: ', enc)
+                #         continue
+                #     except UnicodeError:
+                #         print('Unicode error with enc format: ', enc)
+                #         continue
+                #     except UnicodeDecodeError:
+                #         print('Unicode decode error with enc format: ', enc)
+                #         continue
+                #     except:
+                #         print('File could not be read with enc format: ', enc)
+                #         continue
+                # return result
 
-        table1 = decode(request.files['file1'])
-        table2 = decode(request.files['file2'])
+        # table1 = decode(request.files['file1'])
+        # table2 = decode(request.files['file2'])
+
+        #----- DECODE, 2nd attempt: using chardet module ------
+        def decode(file):
+                check = file.read()
+                file.seek(0)
+                detection = chardet.detect(check)
+                charenc = detection['encoding']
+                print(charenc)
+                return charenc
+
+        def define(file):
+            enc = decode(file)
+            table = pd.read_csv(file, sep='[:;,]', engine='python', encoding=enc)
+            return table
+        
+        table1 = define(request.files['file1'])
+        table2 = define(request.files['file2'])
+
+        #--------------------------------------------------------
 
         writer = pd.ExcelWriter('Comparison.xlsx', engine='xlsxwriter')
 
